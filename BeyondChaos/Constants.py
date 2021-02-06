2bed6c97d7d3e53c115cdc488fed35fe6b637183
micah hawman <micah.hawman@gmail.com>
2021-02-05 22:24:38 -0600

    completed updater

---
+++ BeyondChaos/Constants.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env python3
+Version = "v4.0.2"
+UpdateRequest = "Would you like to check for updates? (y(default)/n)"
+UpdateCheck = "Checking for new updates please wait."
+UpdateFound = "Update found, applying update..."
+UpdateNotFound = "There are no updates at this time"
+UpdateLaunching = "Launching the updater application. This application will now close automatically."
---
+++ BeyondChaos/Update.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import requests
+import shutil
+import Constants
+import time
+import subprocess
+
+#our entry point into the updater, called before we display the GUI to the user
+def update():
+    response = input(Constants.UpdateRequest)
+    #If the user types N or n, we will skip, any other value will update.
+    if response and response[0].lower() == 'n':
+        return
+    else:
+        # Notify the user we are doing things
+        print(Constants.UpdateCheck)
+        #ping github and see if there is a new version released
+        x = requests.get('https://api.github.com/repos/FF6BeyondChaos/BeyondChaos/releases/latest').json()   
+        latestVersion = x['tag_name']
+        currentVersion = Constants.Version
+
+        # We can not have a newer version over an older version if we are checking updater.
+        if latestVersion != currentVersion:
+            #download the latest package
+            print(Constants.UpdateFound)
+            downloadlink = x['assets'][0]['browser_download_url']
+            download_file(downloadlink)
+            print(Constants.UpdateLaunching)
+            subprocess.run("test1.py", shell=True)
+            #wait 3 seconds
+            time.sleep(3)
+            #unzip the file
+            #run updater.exe and sysexit this.
+        else:
+            print(Constants.UpdateNotFound)
+            return;
+
+#saves the file to the directory you ran the exe from, the file will be in a zip format
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    return local_filename
+
+#if response and response[0].lower() == 'y':
+#else
