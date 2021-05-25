fe8c1660b02e5922716d78b04ff885a55a0da471
Doctor Insano <LeonhartSquall93@gmail.com>
2021-05-24 20:48:57 -0400

    Add start of refactoring effort

---
+++ BeyondChaos/Utils/Character/characterreader.py
@@ -0,0 +1,15 @@
+from Dtos.character import Character
+
+
+def read_characters(file_name):
+    character_list = []
+    for i, line in enumerate(open(file_name)):
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+
+        line = " ".join(line.split())
+        c = Character(*line.split(','))
+        c.id = i
+        character_list.append(c)
+    return character_list
--- BeyondChaos/constants.py
+++ BeyondChaos/constants.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
-#plans a lot of the "magic string" text will move into here to make modifying them alot easier on the fly.
+# plans a lot of the "magic string" text will move into here to make modifying them alot easier on the fly.
 
 UpdateFound = "Applying update..."
 UpdateLaunching = "Launching the updater application. This application will now close automatically."
+
+equip_offsets = {"weapon": 15,
+                 "shield": 16,
+                 "helm": 17,
+                 "armor": 18,
+                 "relic1": 19,
+                 "relic2": 20}
+
+char_stat_names = ["hp", "mp", "vigor", "speed", "stamina", "m.power",
+                   "attack", "defense", "m.def", "evade", "mblock"]
