26f3d5bb557575afe0072a59be31143620ee35b4
Doctor Insano <LeonhartSquall93@gmail.com>
2021-06-09 19:50:06 -0400

    Remove broken __repr__ function. Basic cleanup

--- BeyondChaos/Dtos/character.py
+++ BeyondChaos/Dtos/character.py
@@ -1,8 +1,4 @@
-import os
 from typing import List
-
-from constants import char_stat_names
-from utils import make_table
 
 equip_offsets = {"weapon": 15,
                  "shield": 16,
@@ -56,36 +52,5 @@ class Character:
         self.level_modifier_mutated = level_and_run & 0b00001100
         self.run_chance_mutated = level_and_run & 0b00000011
 
-
     def __repr__(self):
-        s = "{0:02d}. {1}".format(self.id + 1, self.newname) + "\n"
-        command_names = []
-        for c in self.command_objs:
-            if c is not None:
-                command_names.append(c.name.lower())
-        s += "Commands: "
-        s += ", ".join(command_names) + "\n"
-        if self.original_appearance and self.new_appearance:
-            s += "Looks like: %s\n" % self.new_appearance
-            s += "Originally: %s\n" % self.original_appearance
-
-        stat_blurbs = {}
-        for name in char_stat_names:
-            blurb = "{0:8} {1}".format(name.upper() + ":", self.stats[name])
-            stat_blurbs[name] = blurb
-        column1 = [stat_blurbs[n] for n in ["hp", "mp", "evade", "mblock"]]
-        column2 = [stat_blurbs[n] for n in ["vigor", "m.power", "speed", "stamina"]]
-        column3 = [stat_blurbs[n] for n in ["attack", "defense", "m.def"]]
-        s += make_table([column1, column2, column3]) + os.linesep
-        # TODO: Refactor Items and make get_notable_equips() work
-        # if self.id < 14:
-        #    s += "Notable equipment: "
-        #    s += ", ".join([n.name for n in self.get_notable_equips()])
-        #    s += "\n"
-        if self.wor_location is not None:
-            s += "World of Ruin location: %s\n" % self.wor_location
-        if self.natural_magic:
-            s += "Has natural magic.\n"
-            for level, spell in self.natural_magic:
-                s += "  LV %s - %s\n" % (level, spell.name)
-        return s.strip()
+        pass
