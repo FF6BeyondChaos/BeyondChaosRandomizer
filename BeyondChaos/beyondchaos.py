c4da90606129b771ce923ce3320ac3d60ae66fcf
DarkSlash88 <43767138+DarkSlash88@users.noreply.github.com>
2023-09-07 20:30:04 -0600

    Update to 5.1.0

--- BeyondChaos/beyondchaos.py
+++ BeyondChaos/beyondchaos.py
@@ -259,7 +259,7 @@ class Window(QMainWindow):
         # values to be sent to Randomizer
         self.romText = ""
         self.romOutputDirectory = ""
-        self.version = "CE-5.0.5"
+        self.version = "CE-5.1.0"
         self.mode = "normal"
         self.seed = ""
         self.flags = []
