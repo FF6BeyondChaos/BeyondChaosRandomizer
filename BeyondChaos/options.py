17e807f7359412c4d62f9c8976f3efa5fbefef7b
DarkSlash88 <43767138+DarkSlash88@users.noreply.github.com>
2024-07-24 20:12:55 -0600

    Fix a few prohibited flags for dragonhunt

--- BeyondChaos/options.py
+++ BeyondChaos/options.py
@@ -490,7 +490,7 @@ ALL_MODES = [
         display_name='Race - Dragon Hunt',
         description="Kill all 8 dragons in the World of Ruin. Intended for racing.",
         forced_flags=["worringtriad"],
-        prohibited_flags={"j", "airship", "alasdraco", "thescenarionottaken", 'rushforpower', 'shadowstays'}
+        prohibited_flags={"j", "alasdraco", "thescenarionottaken", 'rushforpower', 'shadowstays', 'strangejourney'}
     ),
 ]
 
